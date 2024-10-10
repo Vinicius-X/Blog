762d4666004dedeb5ef5edc2650bbeb1a493727b
Vinicius <160507786+Vinicius-X@users.noreply.github.com>
2024-10-10 10:36:43 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Blog da Tati</title>
+</head>
+<body>
+    <h1>Blog do Vinicius</h1>
+    <img src="post.png" alt="imagem com itens de trabalho" srcset="">
+    <p>Colecione momentos</p>
+</body>
+</html>
